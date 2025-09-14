8ae0149a5744fce93a871fcca6f00c35b2748406
Cornelioushandsome <145301075+Cornelioushandsome@users.noreply.github.com>
2025-09-14 14:42:27 -0400

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -1,1 +1,17 @@
+#include "mazeSetup/maze.h"
+#include "genAlgorithms/genAlgorithms.h"
 
+int main(){
+    srand(time(NULL));
+    
+    Maze maze;
+    uint16_t width = 50, height = 50;
+    allocMaze(&maze, width, height);
+
+    generateMazeRecursiveBacktracking(&maze);
+    printMaze(&maze);
+
+    freeMaze(&maze);
+
+    return EXIT_SUCCESS;
+}
